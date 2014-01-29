132f33edd127cf5888f0d480cb195d8784bdfc9a
Felipe <felipe@felipe-HP-ProBook-5330m.(none)>
2014-01-29 10:52:55 -0300

    notices table fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,15 @@
 class Article < ActiveRecord::Base
 	belongs_to :topic
-	has_many :contents
+	has_many :contents, dependent: :destroy
 
-	# has_attached_file :content
+	before_destroy :delete_contents_blobs
 
+	private
+
+	def delete_contents_blobs
+		self.contents.each do |c|
+			c.delete_blob
+		end
+	end
 
 end
