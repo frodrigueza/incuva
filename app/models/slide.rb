02bec2049f44942ca46b302663c5f6ca9764bd90
Felipe <felipe@felipe-HP-ProBook-5330m.(none)>
2014-08-26 16:05:10 -0400

    slides

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -5,13 +5,15 @@ class Slide < ActiveRecord::Base
 		if file_up = params[:file_up]
 			self.image_url = upload_file(file_up)
 		end
-		self.link_url = URI(self.link_url)
 
-		if self.link_url.instance_of?(URI::Generic)
-		    self.link_url = URI::HTTP.build({:host => self.link_url.to_s}) 
+		if params[:link_url] != ''
+			self.link_url = URI(self.link_url)
+			if self.link_url.instance_of?(URI::Generic)
+			    self.link_url = URI::HTTP.build({:host => self.link_url.to_s}) 
+			end
+			self.link_url = self.link_url.to_s
 		end
 
-		self.link_url = self.link_url.to_s
 
 		self.save
 	end
